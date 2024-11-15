f57eedcf30322534de37a4030850899748d3d0a4
Syed Talha <syedtalha.techsurge@gmail.com>
2024-11-15 15:59:14 +0500

    weather show widget

--- AmazonFarmer.Core.Application/DTOs/EngroWeatherAPI.cs
+++ AmazonFarmer.Core.Application/DTOs/EngroWeatherAPI.cs
@@ -131,6 +131,7 @@ namespace AmazonFarmer.Core.Application.DTOs
 
     public class EngroWeatherAPI_Response
     {
+        public bool showWeatherWidget { get; set; } = true;
         public string main { get; set; }
         public string description { get; set; }
         public string weatherIconCode { get; set; }
